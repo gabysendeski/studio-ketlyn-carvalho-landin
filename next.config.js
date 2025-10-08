5a98740d9876bea9485885f64bf9e283bf96202d
Gabrielly Sendeski <gaby.sendeski@hotmail.com>
2025-10-08 13:40:31 -0300

    fix: correct GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const nextConfig = {
   },
   basePath:
     process.env.NODE_ENV === "production"
-      ? "/studio-ketlyn-carvalho-landing"
+      ? "/studio-ketlyn-carvalho-landin"
       : "",
   assetPrefix:
     process.env.NODE_ENV === "production"
-      ? "/studio-ketlyn-carvalho-landing/"
+      ? "/studio-ketlyn-carvalho-landin/"
       : "",
 };
 
